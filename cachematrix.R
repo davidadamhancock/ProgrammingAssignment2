469119ebbeb08779c200bed9b7c2f454c4f03c3a
David Hancock <davidadamhancock@googlemail.com>
2014-10-20 03:47:28 +0100

    Update cachematrix.R
    assignment commit for rprog008 for week 3
    test code added below so unit matrix can prove code

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,28 @@
 ## functions do
 
 ## Write a short comment describing this function
+## set initial variable to cache get initial variable to cache create list for cachesolve
+## I acknowledge reading Sefa Kilic repository before starting to make sure I was on the write track
 
 makeCacheMatrix <- function(x = matrix()) {
+
+	if(is.matrix(yourmatrix)){
+    			ympowerminus1 <- NULL 
+    			setmatrix <- function(y) { 
+        		yourmatrix <<- y 
+        		ympowerminus1  <<- NULL
+			} 
+    	 
+   			getmatrix <- function() yourmatrix 
+    			setmatrixinverse <- function(solve) ympowerminus1 <<- solve 
+    			getmatrixinverse <- function() ympowerminus1
+    			list(set=setmatrix, get=getmatrix, setmatrixinverse=setmatrixinverse, getmatrixinverse=getmatrixinverse) 
+	}
+	else
+		{
+			message("invalid format!")
+			
+		}
 
 }
 
@@ -12,4 +32,35 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+     inv_matrix <- x$getmatrixinverse() 
+     if(!is.null(inv_matrix)) { 
+        message("getting cached data.") 
+        return(inv_matrix) 
+    } 
+    data <- x$get() 
+    inv_matrix <- solve(data) 
+    x$setmatrixinverse(inv_matrix) 
+    inv_matrix
+        
+        
 }
+
+##testcode
+##mymatrix<-rbind(c(1,2),c(2,1))
+##source("makeCacheMatrix.R")
+##vallist<-makeCacheMatrix(mymatrix)
+##source("cacheSolve.R")
+##myinv<-cacheSolve(vallist)
+##myinv%*%mymatrix
+###     [,1] [,2]
+##[1,]    1    0
+##[2,]    0    1
+##
+
+
+
+
+
+
+
+
